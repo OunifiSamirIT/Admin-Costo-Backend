5dd3a2fba86739019ecfedc229f1be8fa55528b2
OunifiSamirIT <ounifisamiritdsi@gmail.com>
2023-12-18 23:18:17 +0000

    vercelCLI

--- src/main.ts
+++ src/main.ts
@@ -8,27 +8,22 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('MAIN');
+
   app.use('/images', express.static(join(__dirname, '..', 'images')));
 
- const corsOptions: CorsOptions = {
-    origin: 'http://localhost:5173', 
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
 
   app.enableCors(corsOptions);
- 
 
-  app.setGlobalPrefix('/api'); 
-  const port = process.env.PORT;
+  app.setGlobalPrefix('/api');
+
+  const port = process.env.PORT || 5000; // Use a default port if not provided
   await app.listen(port);
-  logger.log(`server run on port ${port}`);
-
-
- 
-
-
-
-
+  logger.log(`Server running on port ${port}`);
 }
+
 bootstrap();
